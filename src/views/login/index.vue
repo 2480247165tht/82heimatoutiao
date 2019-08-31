281ded4f48e29dfc844ce8bc20a395d2dd7f744d
谭海婷 <2480247165@qq.com>
2019-08-31 22:04:05 +0800

    登录完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,20 +5,101 @@
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单组件 el-form表单容器 -->
-
       <!-- 数据校验 首先要给el-form 一个model属性 表示数据对象 -->
       <!-- ref 是获取dom元素 -->
-
-      <!-- 表单项  prop绑定需要校验的字段名 但是只写mobile -->
-
-      <!-- 放组件内容 -->
-      <!-- 绑定手机号 -->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
+        <el-form-item prop="mobile">
+          <!-- 表单项  prop绑定需要校验的字段名 但是只写mobile -->
+          <!-- 放组件内容 -->
+          <!-- 绑定手机号 -->
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <!-- 绑定验证码 -->
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:200px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+          <!--绑定是否勾选协议 -->
+          <el-checkbox v-model="loginForm.check">我已经仔细阅读啦</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack()
+      } else {
+        callBack(new Error('您必须的被坑'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [
+          {
+            required: true,
+            message: '手机号不能为空'
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码必须为6位数字'
+          }
+        ],
+        check: [
+          {
+            validator
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(isOk => {
+        if (isOk) {
+          this.$axios({
+            url: '/authorizations',
+            method: 'post',
+            data: this.loginForm
+          })
+            .then(res => {
+              window.localStorage.setItem('user-token', res.data.data.token)
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message({
+                message: '手机号或者验证码错误',
+                type: 'warning'
+              })
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
