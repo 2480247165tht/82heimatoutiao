cad65c93ae9104f3a283e4261a3bebf31446183e
谭海婷 <2480247165@qq.com>
2019-08-31 20:30:20 +0800

    背景和logo

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home'
+import Home from './views/home'
 
 import Login from './views/login'
 
@@ -8,21 +8,22 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home
-  },
-  {
     path: '/login',
     component: Login
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  },
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
   }
+
   ]
 })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="login"></div>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <!-- 表单组件 el-form表单容器 -->
+
+      <!-- 数据校验 首先要给el-form 一个model属性 表示数据对象 -->
+      <!-- ref 是获取dom元素 -->
+
+      <!-- 表单项  prop绑定需要校验的字段名 但是只写mobile -->
+
+      <!-- 放组件内容 -->
+      <!-- 绑定手机号 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
+<style lang='less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 400px;
+    height: 350px;
+    .logo {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
+  }
+}
 </style>
