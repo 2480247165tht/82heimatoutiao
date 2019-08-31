b1d1bf661b3442340d05f0da1de1543f335f844a
谭海婷 <2480247165@qq.com>
2019-08-31 19:38:14 +0800

    新建一个login组件和home组件,并配置路由表,在页面显示

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less' // 引入初始化样式
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值基础地址
+Vue.prototype.$axios = axios // 给 Vue对象的原型属性赋值 那么所有的Vue实例自动拥有$axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
